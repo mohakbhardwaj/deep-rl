4fddce28d7d566162207e6416832e8b8164bbf31
sgautam <shivamgautam@gmail.com>
2016-11-12 00:20:49 -0500

    environments changed for vision/states

--- environments/EnvWrapper.py
+++ environments/EnvWrapper.py
@@ -11,31 +11,42 @@ from collections import deque
 
 class Env(object):
 	"""Wrapper class to for Deep RL with OpenAI gym environments
-	Overloads the step function, maintains a state buffer and preprocesses
+	Implements the step function, maintains a state buffer and preprocesses
 	states"""
 	"""Any other environments implemented must follow OpenAI Gym interfaces to be used with the wrapper"""
-	#Currently supports preprocessing as described in DQN Atari paper
+	#Currently supports vision as described in DQN Atari paper
 
-	def __init__(self, env_name, w_out, h_out, history_length, preprocessing=False):
+	def __init__(self, env_name, w_out, h_out, \
+		history_length, vision =False):
+		
+		'''
+		@param env_name : name of the environment
+		@param vision : image data or state vector
+		@param w_out : width of the final image
+		@param h_out : height of the final image
+		@param history_length : lenght of input in terms of timestep 
+
+		''' 
 		self.env_name = env_name
 		self.w_out = w_out
 		self.h_out = h_out	
-		self.preprocessing = preprocessing #Flag can be set false for environments that do not provide camera frames
+		self.vision = vision #Flag can be set false for environments that do not provide camera frames
 		self.history_length = history_length
 		#The following commands are specific to gym environments,
 		#[TODO]change when different environments are also implemented
 		self.env = gym.make(env_name)
 		self.num_actions = range(self.env.action_space.n)
 		self.state_buffer = deque()
+		self.observation_length =len(self.env.observation_space.high)
 
 	def setSeed(self, seedVal):
 		self.env.seed(seedVal)
 
 	def preprocess_frame(self, obs):
-		"""Preprocessing involves conversion to grayscale and then rescaling to required
+		"""vision involves conversion to grayscale and then rescaling to required
 		dimensions"""
 		processed_frame = obs
-		if self.preprocessing:
+		if self.vision:
 			processed_frame = resize(rgb2gray(obs), (self.h_out, self.w_out))
 		return processed_frame
 
@@ -43,7 +54,7 @@ class Env(object):
 		"""Reset environment to initial state and clears state buffer"""
 		self.state_buffer.clear()
 		xt = self.preprocess_frame(self.env.reset())
-		st = np.stack((xt, xt, xt, xt), axis=0)
+		st = np.stack((xt for i in xrange(self.history_length)), axis=0)
 		[self.state_buffer.append(xt) for i in xrange(self.history_length)]
 		return st
 
@@ -51,20 +62,26 @@ class Env(object):
 		self.env.render()
 
 	def sample_action(self):
+		# Sample random action
 		return self.env.action_space.sample()
 
 	def step(self, action):
 		"""Execute an action in the environment and return the next reward, observation(set of history_length states)
 		and update the current state_buffer"""
 		observation, reward, done, info = self.env.step(action)
-		if self.preprocessing:
+
+		if self.vision:
 			observation = self.preprocess_frame(observation)
 		previous_frames = np.array(self.state_buffer)
 		
 		if len(previous_frames.shape) < 3:
 			previous_frames = previous_frames.reshape(previous_frames.shape[0], 1, previous_frames.shape[1])
 		# print observation
-		s = np.empty((self.history_length, self.h_out, self.w_out))
+		s=np.array([])
+		if self.vision:
+			s = np.empty((self.history_length, self.h_out, self.w_out))
+		else:
+			s= np.empty((self.history_length,self.observation_length,self.observation_length)) 
 		s[:self.history_length, ...] = previous_frames
 		s[self.history_length-1] = observation
 		#Update the state_buffer
--- tests/env_tests/test.py
+++ tests/env_tests/test.py
@@ -9,7 +9,8 @@ from threading import Thread
 import numpy as np
 import time
 
-env = Env('AirRaid-v0', 84, 110, 4, True)
+# env = Env('AirRaid-v0', 84, 110, 4, True)
+env = Env('CartPole-v0', 84, 110, 4, False)
 si = env.reset()
 # print si[-1]
 img = np.zeros([84, 110], dtype=int)
@@ -21,14 +22,25 @@ def display():
 		cv2.imshow("Output", img)
 		cv2.waitKey(33)
 
+
+def run_learner():
+	done =False
+	for _ in range(1000):
+		env.render()
+		observation, reward, done, info = env.step(env.sample_action())
+		img = observation[-1]
+		if done:
+			break
+		time.sleep(0.0002)
+
 t1 = Thread(target=display)
+t2 = Thread(target=run_learner)
 
 t1.start()
+t2.start()
 
-for _ in range(1000):
-	env.render()
-	observation, reward, done, info = env.step(env.sample_action())
-	img = observation[-1]
-	time.sleep(0.0002)
+t1.join()
+t2.join()
+
 
 working = False
