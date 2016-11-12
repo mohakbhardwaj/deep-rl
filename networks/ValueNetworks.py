c03ee8d40d5163fbdd77d5d365f64fc79b42079a
sgautam <shivamgautam@gmail.com>
2016-11-12 03:19:27 -0500

    updated DQN

--- networks/ValueNetworks.py
+++ networks/ValueNetworks.py
@@ -36,13 +36,13 @@ class ValueNetwork(object):
 class ActionValueNetwork(object):
 	def __init__(self, sess, observation_length, action_max, action_min, action_length, discount = 0.90, learning_rate = 0.0001, tau = 0.001, seed = 1234):
 		pass
-	def create_q_network(self):
+	def create_network(self):
 		"""Constructs and initializes core network architecture"""
 		raise NotImplementedError
 	def init_graph(self):
 		"""Overall architecture includingtarget network, gradient ops etc"""
 		raise NotImplementedError
-	def train(self, state_batch, acction_batch, target_batch):
+	def train(self, current_state_batch,target_batch):
 		"""Train using mini-batches as input"""
 		raise NotImplementedError
 	def evaluate_value(self, state_input, action_input):
