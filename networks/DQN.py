6a017efeb0575e1aa921c82d8fb4a14f4fd0bc0b
Shivam <shivamgautam@gmail.com>
2016-12-06 00:41:10 -0500

    Added GPU func

--- networks/DQN.py
+++ networks/DQN.py
@@ -50,9 +50,10 @@ class DQNetwork(ActionValueNetwork):
 
 		# self.model = self.create_network()
 		# self.train_net = self.init_graph()
-		self.graph_ops = self.init_graph()
+		with tf.device('/gpu:0'):
+			self.graph_ops = self.init_graph()
 		# Add an op to initialize the variables.
-		self.init_op = tf.initialize_all_variables()
+			self.init_op = tf.initialize_all_variables()
 		self.sess.run(self.init_op)
 
 		# Add ops to save and restore all the variables.
