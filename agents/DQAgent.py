f4e36f7725f672060533d7ba2e17d032cf17c48d
Mohak Bhardwaj <mohak.bhardwaj@gmail.com>
2016-12-28 00:35:33 -0500

    added initial exploration

--- agents/DQAgent.py
+++ agents/DQAgent.py
@@ -106,93 +106,94 @@ class DQAgent(RLAgent):
       print("Initiate Training")
       # Load params from a saved model
       if self.warm_start:
-          print("Loading parameters")
-          try:
-              self.network.load_params(self.training_params_file)
-              print("Weights Loaded")
-          except:
-              print("Loading failed!")
+        print("Loading parameters")
+        try:
+            self.network.load_params(self.training_params_file)
+            print("Weights Loaded")
+        except:
+            print("Loading failed!")
       while timestep < self.max_training_steps:
-          action = 0
-          if render_env: 
-              self.env.render()
-          if (timestep+1)%self.steps_per_epoch == 0:
-              print("Epoch Done")
-              average_max_q_heldout = 0
-              s_h, _, _, t_h, _ = self.heldout_states.sample_batch(self.batch_size)   
-              for idx, (term,s) in enumerate(zip(t_h,s_h)):
-                  if not term:
-                      q_vals = self.network.evaluate_values(s)
-                      max_q = np.max(q_vals)
-                      average_max_q_heldout += max_q
-              average_max_q_heldout /= self.batch_size
-              log = ["av_max_q", average_max_q_heldout]
-              self.save_log_to_csv(log)
-              print("[INFO]", "Num Episodes Passed: ", num_episodes_passed, "Average Reward Per Episode ", avg_reward, "Episode Reward(unclipped) ", unclipped_episode_reward,\
-               "Average Reward Per Episode(unclipped) ", unclipped_average_reward, "Curr Epsilon: ", self.exploration_strategy.epsilon, "Steps passed: ", timestep,\
-               "Average Max Q (Heldout)", average_max_q_heldout)  
-          #Initially just do random actions till you have enough frmaes to actually update
-          if self.replay_buffer.size() < self.batch_size:
-              action = self.env.sample_action() 
-              next_state, reward, terminal, info = self.env.step(action)
-              self.heldout_states.add(curr_state, action, reward, terminal, next_state)
-          #Otherwise follow the exploration strategy
-          else:
-              best_action = self.network.get_best_action(curr_state)
-              action = self.exploration_strategy.get_action(timestep,best_action)
-              next_state, reward, terminal, info = self.env.step(action)
-              if timestep == self.min_epsilon_timestep:
-                  print self.exploration_strategy.epsilon, self.min_epsilon   
-          unclipped_episode_reward += reward
-          #Clip the reward 
-          if self.clip_reward:
-              reward = self.clip_reward(reward)
-          cumulative_reward += reward
-          #Append the experience to replay buffer
-          self.replay_buffer.add(curr_state, action, reward, terminal, next_state)
-          #Train the network
-          if (self.replay_buffer.size() >= self.start_training_after) and ((timestep%self.gradient_update_frequency) == 0):
-              #Sample a batch form experience buffer
-              #Calculate targets from the batch
-              s_batch, a_batch, r_batch, t_batch, s2_batch = self.replay_buffer.sample_batch(self.batch_size) 
-              #While calculating targets if some state is terminal, then target must be R(s) and not 
-              # R(s) + gamma x Q*(s',a')  
-              target_batch = r_batch
-              for idx, (term,s2,target) in enumerate(zip(t_batch,s2_batch,target_batch)):
-                  lookahead = 0
-                  if not term:
-                      #Target q values are obtained from target network
-                      q_vals = self.network.evaluate_values_target(s2)
-                      max_q = np.max(q_vals)
-                      lookahead = self.discount_factor * max_q
-                  target_batch[idx] += lookahead 
-              #Send the state batch and target batch to DQN
-              self.network.train(s_batch, target_batch, a_batch)
-          # Periodically update target network
-          if (timestep % self.target_network_update) == 0:  
-            print("[INFO", "Timestep: ", timestep," Updating target network params")
-            self.network.update_target_network_params()
-          
-          if terminal:
-              #Begin a new episode if reached terminal episode
-              curr_state = self.env.reset()
-              #Update the average reward
-              num_episodes_passed += 1
-              unclipped_average_reward += (unclipped_episode_reward - unclipped_average_reward)/num_episodes_passed
-              avg_reward += (cumulative_reward - avg_reward)/num_episodes_passed
-              log_info= [num_episodes_passed, cumulative_reward, episode_length, avg_reward, unclipped_episode_reward,unclipped_average_reward]
-              self.save_log_to_csv(log_info)
-              if (num_episodes_passed + 1)%self.save_after_episodes == 0:
-                  print("Saving currently learned weights")
-                  self.network.save_params(self.training_params_file)
-              #Reset episode statistics               
-              cumulative_reward = 0
-              unclipped_episode_reward = 0
-              episode_length = 0
-          else:
-              curr_state = next_state
-              episode_length += 1
-          timestep += 1           
+        action = 0
+        if render_env: 
+            self.env.render()
+        if (timestep+1)%self.steps_per_epoch == 0:
+            print("Epoch Done")
+            average_max_q_heldout = 0
+            s_h, _, _, t_h, _ = self.heldout_states.sample_batch(self.batch_size)   
+            for idx, (term,s) in enumerate(zip(t_h,s_h)):
+                if not term:
+                    q_vals = self.network.evaluate_values(s)
+                    max_q = np.max(q_vals)
+                    average_max_q_heldout += max_q
+            average_max_q_heldout /= self.batch_size
+            log = ["av_max_q", average_max_q_heldout]
+            self.save_log_to_csv(log)
+            print("[INFO]", "Num Episodes Passed: ", num_episodes_passed, "Average Reward Per Episode ", avg_reward, "Episode Reward(unclipped) ", unclipped_episode_reward,\
+             "Average Reward Per Episode(unclipped) ", unclipped_average_reward, "Curr Epsilon: ", self.exploration_strategy.epsilon, "Steps passed: ", timestep,\
+             "Average Max Q (Heldout)", average_max_q_heldout)  
+        #Initially execute uniform random policy and populate the experience buffer
+        if timestep < self.start_training_after:
+          action = self.env.sample_action()         
+        #Otherwise follow the exploration strategy
+        else:
+          best_action = self.network.get_best_action(curr_state)
+          action = self.exploration_strategy.get_action(timestep,best_action)
+        
+        next_state, reward, terminal, info = self.env.step(action)  
+        
+        if self.replay_buffer.size() < self.batch_size:        
+          self.heldout_states.add(curr_state, action, reward, terminal, next_state)
+        
+        unclipped_episode_reward += reward
+        #Clip the reward 
+        if self.clip_reward:
+            reward = self.clip_reward(reward)
+        cumulative_reward += reward
+        #Append the experience to replay buffer
+        self.replay_buffer.add(curr_state, action, reward, terminal, next_state)
+        #Train the network
+        if (self.replay_buffer.size() >= self.start_training_after) and ((timestep%self.gradient_update_frequency) == 0):
+            #Sample a batch form experience buffer
+            #Calculate targets from the batch
+            s_batch, a_batch, r_batch, t_batch, s2_batch = self.replay_buffer.sample_batch(self.batch_size) 
+            #While calculating targets if some state is terminal, then target must be R(s) and not 
+            # R(s) + gamma x Q*(s',a')  
+            target_batch = r_batch
+            for idx, (term,s2,target) in enumerate(zip(t_batch,s2_batch,target_batch)):
+                lookahead = 0
+                if not term:
+                    #Target q values are obtained from target network
+                    q_vals = self.network.evaluate_values_target(s2)
+                    max_q = np.max(q_vals)
+                    lookahead = self.discount_factor * max_q
+                target_batch[idx] += lookahead 
+            #Send the state batch and target batch to DQN
+            self.network.train(s_batch, target_batch, a_batch)
+        # Periodically update target network
+        if (timestep % self.target_network_update) == 0:  
+          print("[INFO", "Timestep: ", timestep," Updating target network params")
+          self.network.update_target_network_params()
+        
+        if terminal:
+            #Begin a new episode if reached terminal episode
+            curr_state = self.env.reset()
+            #Update the average reward
+            num_episodes_passed += 1
+            unclipped_average_reward += (unclipped_episode_reward - unclipped_average_reward)/num_episodes_passed
+            avg_reward += (cumulative_reward - avg_reward)/num_episodes_passed
+            log_info= [num_episodes_passed, cumulative_reward, episode_length, avg_reward, unclipped_episode_reward,unclipped_average_reward]
+            self.save_log_to_csv(log_info)
+            if (num_episodes_passed + 1)%self.save_after_episodes == 0:
+                print("Saving currently learned weights")
+                self.network.save_params(self.training_params_file)
+            #Reset episode statistics               
+            cumulative_reward = 0
+            unclipped_episode_reward = 0
+            episode_length = 0
+        else:
+            curr_state = next_state
+            episode_length += 1
+        timestep += 1           
       #Save final model weights after traning complete
       print("Training Done. Saving final model weights")
       self.network.save_params(self.training_params_file)
